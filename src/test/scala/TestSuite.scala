25c61a4f93bde003634fb38143ba2245ebf2e307
Arthur Lui <luiarthur@gmail.com>
2016-08-20 21:15:41 -0700

    yes

--- src/test/scala/TestSuite.scala
+++ src/test/scala/TestSuite.scala
@@ -14,10 +14,10 @@ class TestSuite extends FunSuite {
     val img = ImageIO.read(new File("src/test/resources/img/sample.jpg"))
     for (i <- 0 until img.getHeight; j <- 0 until img.getWidth) {
       val c = new Color(img.getRGB(j,i))
-      val x = if (c.getRed < 50) 0 else 255
+      val x = { if ( c.getRed+ c.getGreen + c.getBlue > 70*3.0) 255 else 0 }
       val newc = new Color(x,x,x)
       img.setRGB(j,i,newc.getRGB)
     }
-    ImageIO.write(img, "jpg", new File("src/test/resources/img/grayscale.jpg"));
+    ImageIO.write(img, "jpg", new File("src/test/resources/img/grayscale.jpg"))
   }
 }
