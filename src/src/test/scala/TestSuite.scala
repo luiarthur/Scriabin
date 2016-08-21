3e81994b4d629128f291d3c07079b1467f2c0781
Arthur Lui <luiarthur@gmail.com>
2016-08-20 18:03:17 -0700

    adding resource files

--- src/src/test/scala/TestSuite.scala
+++ src/src/test/scala/TestSuite.scala
@@ -2,6 +2,6 @@ import org.scalatest.FunSuite
 
 class TestSuite extends FunSuite {
   test("Test TestSuite") {
-    assert(true)
+    val sampletxt = scala.io.Source.fromFile("src/test/resources/sample.txt")
   }
 }
