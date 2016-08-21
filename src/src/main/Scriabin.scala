a8e0d283a52333479f683562dd8032761619f1b9
Arthur Lui <luiarthur@gmail.com>
2016-08-20 20:48:49 -0700

    yes

--- src/src/main/Scriabin.scala
+++
@@ -1,4 +0,0 @@
-package scriabin
-
-object Scriabin {
-}
--- src/src/test/scala/TestSuite.scala
+++
@@ -1,7 +0,0 @@
-import org.scalatest.FunSuite
-
-class TestSuite extends FunSuite {
-  test("Test TestSuite") {
-    val sampletxt = scala.io.Source.fromFile("src/test/resources/sample.txt")
-  }
-}
